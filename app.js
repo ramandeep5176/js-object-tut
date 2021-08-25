c6834f0a08a59a927d402f9319c5a1517bca8c0d
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-08-25 17:26:51 -0400

    second update

---
+++ app.js
@@ -0,0 +1,19 @@
+const person = {
+  name: "john",
+  age: 25,
+  married: true,
+  siblings: ["anna", "bobo"],
+  greet: function (name) {
+    console.log(`hello i'm ${name}`);
+  },
+  sayhello(name) {
+    console.log(`hello i'm ${name}`);
+  },
+  job: {
+    title: "developer",
+    conpany: {
+      name: "gill",
+      lastname: "gilly",
+    },
+  },
+};
