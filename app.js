3f561d2ce7aa5f4d52d7da708e3ed72a897689dd
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-08-25 17:45:16 -0400

    object commented

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-const person = {
-  name: "john",
-  age: 25,
-  married: true,
-  siblings: ["anna", "bobo"],
-  greet: function (name) {
-    console.log(`hello i'm ${name}`);
-  },
-  sayhello(name) {
-    console.log(`hello i'm ${name}`);
-  },
-  job: {
-    title: "developer",
-    conpany: {
-      name: "gill",
-      lastname: "gilly",
-    },
-  },
-};
+// d
+// e
+
+// const age = 60;
+// let random = "random-value";
+// const person = {
+//   name: "john",
+//   age: 25,
+//   married: true,
+//   siblings: ["anna", "bobo"],
+//   greet: function (name) {
+//     console.log(`hello i'm ${name}`);
+//   },
+//   sayhello(name) {
+//     console.log(`hello i'm ${name}`);
+//   },
+//   job: {
+//     title: "developer",
+//     conpany: {
+//       name: "gill",
+//       lastname: {
+//         kam: age,
+//       },
+//     },
+//   },
+//   "random-value": "random",
+// };
+// console.log(person);
+// console.log(person.job.conpany.name);
+// console.log(person.job.conpany.lastname.kam);
+// console.log(person["random-value"]);
+// console.log(person[random]);
+
